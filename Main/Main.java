fb7c2e3a590fae699884fc56cf87e921ebf47654
Nikita <manoshkinns@mail.ru>
2022-10-22 05:17:08 +0600

    Update Main.java
    Finished (but need to do some refactoring)

--- Main/Main.java
+++ Main/Main.java
@@ -13,9 +13,8 @@ public class Main {
         int[] salariesInSecondStack = new int[amountOfCVsInSecondStack];
         fillStacks(amountOfCVsInFirstStack, amountOfCVsInSecondStack, salariesInFirstStack, salariesInSecondStack,
                 scanner);
-        int sumOfSalaries = calculateSumOfSalaries(maxAmountOfSalaries, salariesInFirstStack, salariesInSecondStack,
-                amountOfCVsInFirstStack, amountOfCVsInSecondStack);
-        System.out.println(sumOfSalaries);
+        int answer = calculateLargestSumOfElementsInEachStack(salariesInFirstStack, salariesInSecondStack, maxAmountOfSalaries);
+        System.out.println(answer);
     }
     public static void fillStacks (int amountOfCVsInFirstStack, int amountOfCVsInSecondStack,
                                    int[] salariesInFirstStack, int[] salariesInSecondStack, Scanner scanner) {
@@ -36,8 +35,7 @@ public class Main {
         }
     }
     public static int calculateSumOfSalaries (int maxAmountOfSalaries, int[] salariesInFirstStack,
-                                               int[] salariesInSecondStack, int amountOfCVsInFirstStack,
-                                               int amountOfCVsInSecondStack) {
+                                               int[] salariesInSecondStack) {
         int sum = 0;
         int i = 0;
         int j = 0;
@@ -71,5 +69,114 @@ public class Main {
 
     public static int findMaxAmountOfCVs (int amountOfCVsInFirstStack, int amountOfCVsInSecondStack) {
         return Math.max(amountOfCVsInFirstStack, amountOfCVsInSecondStack);
+    }
+
+    public static int calculateLargestSumOfElementsInEachStack (int[] firstStack, int[] secondStack, int maxSum) {
+        //Separate counters and summing
+        int sumInFirstStack = 0;
+        int sumInSecondStack = 0;
+        int sumOfBothStacksOneByOne = 0;
+        int counter1;
+        int counter2;
+        int counter3 = 0;
+        for (counter1 = 0; counter1 < firstStack.length; counter1++) {
+            if (sumInFirstStack + firstStack[counter1] <= maxSum) {
+                sumInFirstStack += firstStack[counter1];
+            } else {
+                break;
+            }
+        }
+        for (counter2 = 0; counter2 < secondStack.length; counter2++) {
+            if  (sumInSecondStack + secondStack[counter2] <= maxSum) {
+                sumInSecondStack += secondStack[counter2];
+            } else {
+                break;
+            }
+        }
+        for (int j = 0; true; j++) {
+            if (firstStack[j] < secondStack[j]) {
+                if (sumOfBothStacksOneByOne + firstStack[j] <= maxSum) {
+                    sumOfBothStacksOneByOne += firstStack[j];
+                    counter3++;
+                } else {
+                    break;
+                }
+                if (sumOfBothStacksOneByOne + secondStack[j] <= maxSum) {
+                    sumOfBothStacksOneByOne += secondStack[j];
+                    counter3++;
+                } else {
+                    break;
+                }
+            } else {
+                if (sumOfBothStacksOneByOne + secondStack[j] <= maxSum) {
+                    sumOfBothStacksOneByOne += secondStack[j];
+                    counter3++;
+                } else {
+                    break;
+                }
+                if (sumOfBothStacksOneByOne + firstStack[j] <= maxSum) {
+                    sumOfBothStacksOneByOne += firstStack[j];
+                    counter3++;
+                } else {
+                    break;
+                }
+            }
+        }
+        if (counter1 >= counter2 && counter1 >= counter3) {
+            return calculateMaxAmountOfCVsStartFromFirstStack(maxSum, firstStack, secondStack);
+        } else if (counter2 > counter1 && counter2 > counter3) {
+            return calculateMaxAmountOfCVsStartFromSecondStack(maxSum, firstStack, secondStack);
+        } else {
+            return calculateSumOfSalaries(maxSum, firstStack, secondStack);
+        }
+    }
+    public static int calculateMaxAmountOfCVsStartFromFirstStack(int maxAmountOfSalaries, int[] salariesInFirstStack,
+                                                                  int[] salariesInSecondStack) {
+        int sum = 0;
+        int numberOfCVs = 0;
+        while (sum < maxAmountOfSalaries) {
+            for (int j : salariesInFirstStack) {
+                if ((sum + j) <= maxAmountOfSalaries) {
+                    sum += j;
+                    numberOfCVs++;
+                } else {
+                    return numberOfCVs;
+                }
+            }
+            for (int j : salariesInSecondStack) {
+                if ((sum + j) <= maxAmountOfSalaries) {
+                    sum += j;
+                    numberOfCVs++;
+                } else {
+                    return numberOfCVs;
+                }
+            }
+        }
+        return numberOfCVs;
+    }
+
+    public static int calculateMaxAmountOfCVsStartFromSecondStack(int maxAmountOfSalaries, int[] salariesInFirstStack,
+                                                                 int[] salariesInSecondStack) {
+        int sum = 0;
+        int numberOfCVs = 0;
+        while (sum < maxAmountOfSalaries) {
+            for (int j : salariesInSecondStack) {
+                if ((sum + j) <= maxAmountOfSalaries) {
+                    sum += j;
+                    numberOfCVs++;
+                } else {
+                    return numberOfCVs;
+                }
+            }
+            for (int j : salariesInFirstStack) {
+                if ((sum + j) <= maxAmountOfSalaries) {
+                    sum += j;
+                    numberOfCVs++;
+                } else {
+                    return numberOfCVs;
+                }
+            }
+        }
+        return numberOfCVs;
     }
 }
